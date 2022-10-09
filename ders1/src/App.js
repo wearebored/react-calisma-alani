dc01731f1fdb957b5354e84a02b57eeaffc561e4
enes <enesooo78@gmail.com>
2022-10-10 00:16:19 +0300

    a

--- ders1/src/App.js
+++ ders1/src/App.js
@@ -1,11 +1,21 @@
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{backgroundColor:"red", color:"white", textAlign:"center"}}>
       <h1>hello</h1>
       <h1>maraba</h1>
+      <h1>react</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel, quae!</p>
+      <div>
+        <img src="./logo192.png" alt="" />
+      </div>
     </div>
   );
+}
+export function Yeni(){
+  return(
+    <div><h1>yapıldı</h1></div>
+  )
 }
 
 export default App;
--- ders1/src/index.js
+++ ders1/src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import App, { Yeni } from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const root2 = ReactDOM.createRoot(document.getElementById("root2"));
+root.render( <Yeni/>);
+root2.render(<App />);
 
 
