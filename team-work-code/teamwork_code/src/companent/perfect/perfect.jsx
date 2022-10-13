b2785624617a0a83d99417bd0b5300e85bd1c96b
enes <enesooo78@gmail.com>
2022-10-13 20:20:31 +0300

    perfect hazır

---
+++ team-work-code/teamwork_code/src/companent/perfect/perfect.jsx
@@ -0,0 +1,30 @@
+function Perfect(a,b){
+    const ilksayi = a;
+    const ikincisayi = b;
+    let asal = [];
+    let liste = [];
+    let arasinda = [];
+    for (let i = 2; i < ikincisayi + 1; i++) {
+      let sayac = 0;
+      for (let a = 2; a <= i; a++) {
+        if (i % a === 0) {
+          sayac += 1;
+        }
+      }
+      if (sayac < 2) {
+        asal.push(i);
+      }
+      if (asal.length === ikincisayi.toString().length) {
+        break;
+      }
+    }
+    for (let i = 0; i < asal.length; i++) {
+      const formul = 2 ** (asal[i] - 1) * (2 ** asal[i] - 1);
+      liste.push(formul);
+    }
+    for (let i of liste) {
+      if (i >= ilksayi && i <= ikincisayi) {
+        arasinda.push(i);
+      }
+    }
+}
