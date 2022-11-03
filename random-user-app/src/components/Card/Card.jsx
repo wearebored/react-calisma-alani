d6a3b6990fdc4a94b6481a9a7dbbb529772799f8
enes <enesooo78@gmail.com>
2022-11-03 03:24:58 +0300

    css hazırlık

---
+++ random-user-app/src/components/Card/Card.jsx
@@ -0,0 +1,47 @@
+function Card({ userdata }) {
+  console.log(userdata);
+//   let registereDate = new Date(Date.parse(userdata.registered?.date));
+  
+  return (
+    <div>
+      <div className="picturename">
+        <img src={userdata.picture?.large} alt={userdata.name?.last} />
+        <p>
+          {userdata.name?.title +
+            " " +
+            userdata.name?.first +
+            " " +
+            userdata.name?.last}
+        </p>
+      </div>
+      <div className="mail">
+        <i className="fa-solid fa-envelopes-bulk"></i>
+        <p>{userdata.email}</p>
+      </div>
+      <div className="phone">
+        <i className="fa-solid fa-phone"></i>
+        <p>{userdata.phone}</p>
+      </div>
+      <div className="location">
+        <i className="fa-solid fa-map-location-dot"></i>
+        <p>{userdata.location?.city + " - " + userdata.location?.country}</p>
+      </div>
+      <div className="age">
+        <p>Age:{userdata.dob?.age}</p>
+      </div>
+      <div className="date">
+        <p>
+          {/* Register Date:
+          {registereDate.getFullYear() +
+            "-" +
+            (registereDate.getMonth() + 1) +
+            "-" +
+            registereDate.getDate()} */}
+          Register Date:{userdata.registered?.date.slice(0,10)}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
---
+++ random-user-app/src/components/Header/Header.jsx
@@ -0,0 +1,37 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Card from "../Card/Card";
+
+function Header() {
+  const [userdata, setUserdata] = useState("");
+  useEffect(() => {
+    data();
+  }, []);
+
+  const data = async () => {
+    const url = "https://randomuser.me/api/";
+    try {
+      const veri = await axios.get(url);
+      if (veri.status === 200) {
+        setUserdata(veri.data.results[0]);
+        
+      } else {
+        // throw "veri hatalı geldi";
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const randomClick = () => {
+    data();
+  };
+
+  return (
+    <div>
+      <Card userdata={userdata} />
+      <button onClick={randomClick}>Random User</button>
+    </div>
+  );
+}
+
+export default Header;
